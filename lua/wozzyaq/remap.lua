addfe68bba5e40a1c57e9d977740e501460db033
Vladyslav Chaikovskyi <chaikovskyiivv@gmail.com>
2024-11-10 03:29:10 +0200

    chore: update remap with description

--- lua/wozzyaq/remap.lua
+++ lua/wozzyaq/remap.lua
@@ -1,18 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- yank to clipboard
-vim.keymap.set('', '<leader>y', '"+y')
--- yank until EOL to clipboard
-vim.keymap.set('', '<leader>Y', '"+y$')
--- paste after cursor from clipboard
-vim.keymap.set('n', '<leader>p', '"+p')
--- paste before cursor from clipboard
-vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('', '<leader>y', '"+y', { desc = 'Yank to clipboard' })
+vim.keymap.set('', '<leader>Y', '"+y$', { desc = 'Yank until EOL to clipboard' })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste after cursor from clipboard' })
+vim.keymap.set('n', '<leader>P', '"+P', { desc = 'Paste before cursor from clipboard' })
 
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
-
-
 
